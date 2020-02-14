686df0e94928f6782e49ff1ed98acfc6685b9ef0
canesche <michael.canesche@gmail.com>
2020-02-14 10:00:53 -0300

    update pos_node

--- scripts/pos_node.py
+++ scripts/pos_node.py
@@ -1,5 +1,8 @@
 import sys
 import networkx as nx
+import matplotlib.pyplot as plt
+import numpy as np
+
 from graphviz import Source
 
 if __name__ == "__main__":
@@ -14,7 +17,19 @@ if __name__ == "__main__":
 	
     type_graph = ['dot', 'neato', 'fdp', 'sfdp', 'twopi', 'circo']
 
-    pos = nx.drawing.nx_agraph.graphviz_layout(g, prog=type_graph[1])
+    get_position = nx.drawing.nx_agraph.graphviz_layout(g, prog=type_graph[1])
 
-    print(pos)
+    label, x_pos, y_pos = [], [], []
+    for i in get_position:
+        label.append(i)
+        x_pos.append(get_position[i][0])
+        y_pos.append(get_position[i][1])
+        print("Node: " + i +" x = "+ str(get_position[i][0]) +" y = "+ str(get_position[i][1]))
 
+    fig, ax = plt.subplots()
+    ax.scatter(x_pos, y_pos)
+
+    for i, txt in enumerate(label):
+        ax.annotate(txt, (x_pos[i], y_pos[i]))
+
+    plt.show()
