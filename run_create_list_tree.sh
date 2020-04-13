1bfb3e051b9e62f2065af25419ddfd33eeec3bf8
canesche <michael.canesche@gmail.com>
2020-04-13 08:53:57 -0300

    update

--- run_create_list_tree.sh
+++ run_create_list_tree.sh
@@ -4,5 +4,5 @@ GRAPH=('tree_n_15_t_1' 'tree_n_15_t_2' 'tree_n_15_t_3' 'tree_n_15_t_4'
 
 for ((i=0; i < ${#GRAPH[@]}; i++)) do
     echo "GRAPH "${GRAPH[i]}" TO LIST"
-    python3 scripts/dot_to_list_no_dic.py dot/${GRAPH[i]}.dot > list/${GRAPH[i]}_zigzag.in 
+    python3 scripts/dot_to_list.py dot/${GRAPH[i]}.dot > list/${GRAPH[i]}_zigzag.in 
 done
