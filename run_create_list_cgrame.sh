e231a62f3f564524385fa7117950f7c3cddf805c
canesche <michael.canesche@gmail.com>
2020-04-15 11:33:51 -0300

    list cgrame Westerley

---
+++ run_create_list_cgrame.sh
@@ -0,0 +1,7 @@
+GRAPH=('accumulate' 'cap' 'conv2' 'conv3' 'mac' 'mac2' 'matrixmultiply' 'mults1'
+       'mults2' 'nomem1' 'simple2' 'sum' 'twoloops1' 'twoloops2')
+
+for ((i=0; i < ${#GRAPH[@]}; i++)) do
+    echo "GRAPH "${GRAPH[i]}" TO LIST"
+    python3 scripts/dot_to_list.py dot_cgrame/cgrame_${GRAPH[i]}.dot > list_cgrame/${GRAPH[i]}_zigzag.in 
+done
