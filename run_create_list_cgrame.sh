cf4b395562e98ccfec3a22c4ca6c977b247e321a
canesche <michael.canesche@gmail.com>
2020-04-15 16:09:27 -0300

    update

--- run_create_list_cgrame.sh
+++ run_create_list_cgrame.sh
@@ -3,5 +3,5 @@ GRAPH=('accumulate' 'cap' 'conv2' 'conv3' 'mac' 'mac2' 'matrixmultiply' 'mults1'
 
 for ((i=0; i < ${#GRAPH[@]}; i++)) do
     echo "GRAPH "${GRAPH[i]}" TO LIST"
-    python3 scripts/dot_to_list.py dot_cgrame/cgrame_${GRAPH[i]}.dot > list_cgrame/${GRAPH[i]}_zigzag.in 
+    python3 scripts/dot_to_list.py dot_cgrame/cgrame_${GRAPH[i]}.dot > list_cgrame/cgrame_${GRAPH[i]}_zigzag.in 
 done
