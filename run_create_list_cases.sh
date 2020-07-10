3d8d003726c51bdfe2c54c51e2a4e2581a96dff2
canesche <michael.canesche@gmail.com>
2020-07-10 14:00:16 -0300

    update

---
+++ run_create_list_cases.sh
@@ -0,0 +1,37 @@
+
+
+GRAPH=(
+'Cplx8'
+'FilterRGB'
+'Fir16'
+'arf'
+'collapse_pyr'
+'conv3'
+'cosine1'
+'cosine2'
+'ewf'
+'feedback_points'
+'fir1'
+'fir2'
+'h2v2_smooth'
+'hal'
+'horner_bezier_surf'
+'idctcol'
+'interpolate_aux'
+'invert_matrix'
+'jpeg_fdct_islow'
+'jpeg_idct_ifast'
+'k4n4op'
+'mac'
+'matmul'
+'motion_vectors'
+'mults1'
+'simple'
+'smooth_color_z'
+'write_bmp_header'
+)
+
+for ((i=0; i < ${#GRAPH[@]}; i++)) do
+    echo "GRAPH "${GRAPH[i]}" TO LIST"
+    python3 scripts/dot_to_list.py dot_cases/${GRAPH[i]}.dot > list_cases/${GRAPH[i]}_zigzag.in 
+done
